80cab334f5dca1815202b1936fe2c19558b36438
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-09-18 22:37:25 -0700

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -3,18 +3,12 @@ fs.readFile('index.html', (err, data) => html = data);
 
 
 require('http').createServer(function (req, res) {
-//   var validFile, fd;
-//   fs.readFile(req.url.substring(1), (err, data) => {
-//     validFile = !error
-//     if (validFile)
-//       fd = data;
-//   });
-  if (req.url.contains('.js')){
-    var fd;
+  if (req.url == '/')
+    res.end(html);
+  else{
     fs.readFile(req.url.substring(1), (err, data) => {
       fd = data;
     });
     res.end(data);
-  } else
-    res.end(html);
+  }
 }).listen(80);
