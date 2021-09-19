4affb7b95e9341d208aaf89fab76f2f77bef728d
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-09-18 18:09:55 -0700

    Create server.js

---
+++ dist/server.js
@@ -0,0 +1,18 @@
+var app, html, fs = require('fs');
+fs.readFile('index.html', (err, data) => html = data);
+fs.readFile('app.js', (err, data) => app = data);
+
+
+require('http').createServer(function (req, res) {
+  switch (req.url) {
+    case '/':
+      res.writeHead(302, {'Location': Math.random().toString().split(2)});
+      res.end();
+      break;
+    case '/app.js':
+      res.end(app);
+      break;
+    default:
+      res.end('index.html');
+  }
+})
