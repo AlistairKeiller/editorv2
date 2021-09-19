23cc4fd9b3803e3f04fac71f381a7bd5f33b27fc
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-09-18 21:59:25 -0700

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -4,15 +4,16 @@ fs.readFile('app.js', (err, data) => app = data);
 
 
 require('http').createServer(function (req, res) {
-  switch (req.url) {
-    case '/':
-      res.writeHead(302, {'Location': Math.random().toString().split(2)});
-      res.end();
-      break;
-    case '/app.js':
-      res.end(app);
-      break;
-    default:
-      res.end(html);
-  }
+//   switch (req.url) {
+//     case '/':
+//       res.writeHead(302, {'Location': Math.random().toString().split(2)});
+//       res.end();
+//       break;
+//     case '/app.js':
+//       res.end(app);
+//       break;
+//     default:
+//       res.end(html);
+//   }
+  res.end('html');
 }).listen(80);
