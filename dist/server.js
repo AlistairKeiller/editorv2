378f0b7f2bb94779ba52bb5a3fda544358f1f098
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-09-18 22:28:48 -0700

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -3,10 +3,10 @@ fs.readFile('index.html', (err, data) => html = data);
 
 
 require('http').createServer(function (req, res) {
-  var fd;
-  fs.readFile(req.url.substring(1), (err, data) => fd = data);
-  if (fd)
-    res.end(fd);
-  else
+//   var fd;
+//   fs.readFile(req.url.substring(1), (err, data) => fd = data);
+//   if (fd)
+//     res.end(fd);
+//   else
     res.end(html);
 }).listen(80);
