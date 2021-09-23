83545539b62b6d8afaeb8e9543e02594b69e74ae
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-09-23 13:28:50 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { Doc } from 'yjs';
 import { WebrtcProvider } from 'y-webrtc';
 import { MonacoBinding } from 'y-monaco';
 import { editor } from 'monaco-editor';
-import * as BrowserFS from 'browserfs';
 
 const ydoc = new Doc();
 const provider = new WebrtcProvider(window.location.pathname, ydoc);
@@ -23,14 +22,3 @@ const monacoBinding = new MonacoBinding(
   new Set([mEditor]),
   provider.awareness
 );
-
-BrowserFS.configure({
-  fs: "MountableFileSystem",
-  options: {
-    '/home': { fs: 'LocalStorage' },
-    '/tmp': { fs: 'InMemory' }
-  }
-}, function(e) {
-  if (e)
-    throw e;
-});
