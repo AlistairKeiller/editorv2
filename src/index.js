042ddae02ae035aa254d759bee5be6ce87bb2b23
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-09-27 09:40:12 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Doc } from 'yjs';
 import { WebrtcProvider } from 'y-webrtc';
 import { MonacoBinding } from 'y-monaco';
 import { editor } from 'monaco-editor';
+import * as BrowserFS from 'browserfs';
 
 const ydoc = new Doc();
 const provider = new WebrtcProvider(window.location.pathname, ydoc);
@@ -22,3 +23,23 @@ const monacoBinding = new MonacoBinding(
   new Set([mEditor]),
   provider.awareness
 );
+
+fetch('mydata.zip').then(function(response) {
+  return response.arrayBuffer();
+}).then(function(zipData) {
+  BrowserFS.configure({
+    fs: "MountableFileSystem",
+    options: {
+      "/zip": {
+        fs: "ZipFS",
+        options: {
+          zipData: BrowserFS.BFSRequire('buffer').Buffer.from(zipData)
+        }
+      },
+      "/home": { fs: "InMemory" }
+    }
+  }, function(e) {
+    if (e)
+      throw e;
+  });
+});
