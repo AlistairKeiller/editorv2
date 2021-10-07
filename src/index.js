b7ef8193d6b27183bcf8e2f68305a8c9733cccc4
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-10-07 15:50:57 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { MonacoBinding } from 'y-monaco';
 import { editor } from 'monaco-editor';
 import * as BrowserFS from 'browserfs';
 import * as Doppio from 'doppiojvm';
-import 'core-js';
 
 const ydoc = new Doc();
 const provider = new WebrtcProvider(window.location.pathname, ydoc);
@@ -26,20 +25,46 @@ const monacoBinding = new MonacoBinding(
   provider.awareness
 );
 
-fetch('doppio_home.zip')
-  .then((r) => r.arrayBuffer())
-  .then((d) => {
-    BrowserFS.configure({
-      fs: "MountableFileSystem",
-      options: {
-        "/home": {
-          fs: "ZipFS",
-          options: {
-            zipData: BrowserFS.BFSRequire('buffer').Buffer.from(d)
-          }
-        },
-        "/tmp": { fs: "InMemory" }
-      }
-    }, (e) => {if (e) throw e;});
-    new Doppio.VM.JVM({doppioHomePath: '/home'});
+fs = BrowserFS.BFSRequire('fs'), path = BrowserFS.BFSRequire('path'), Buffer = BrowserFS.BFSRequire('buffer').Buffer, process = BrowserFS.BFSRequire('process');
+process.initializeTTYs();
+process.stdout.on('data', function(data) {
+  console.log(data.toString());
+});
+process.stderr.on('data', function(data) {
+  console.log(data.toString());
+});
+function copyDir(src, dest) {
+  fs.mkdir(dest, e => {
+    if (e && e.code !== 'EEXIST')
+      console.log(e);
+    fs.readdir(src, (e, files) => {
+      if (e)
+        console.log(e);
+      files.forEach(file => {
+        srcFile = path.resolve(src, file), destFile = path.resolve(dest, file);
+        fs.stat(srcFile, (e, stat) => {
+          if (e)
+            console.log(e);
+          stat.isDirectory() ?
+          copyDir(srcFile, destFile) :
+          fs.readFile(srcFile, (e, data) => {
+            if (e)
+              console.log(e);
+            fs.writeFile(destFile, data);
+          });
+        });
+      });
+    });
   });
+}
+fetch('doppio_home.zip')
+  .then(r => r.arrayBuffer())
+  .then(d => {
+    mfs = new BrowserFS.FileSystem.MountableFileSystem();
+    mfs.mount('/zip_home', new BrowserFS.FileSystem.ZipFS(new Buffer(d)));
+    mfs.mount('/home', new BrowserFS.FileSystem.InMemory());
+    mfs.mount('/tmp', new BrowserFS.FileSystem.InMemory());
+    BrowserFS.initialize(mfs);
+
+    copyDir('/zip_home', '/home');
+    });
