5b02d934bf6f2ed630fb25b2dba109294bb22a68
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-10-07 18:25:53 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin'),
-path = require('path');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin'), path = require('path'), webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -15,7 +14,18 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    alias: {
+      'fs': 'browserfs/dist/shims/fs.js',
+      'buffer': 'browserfs/dist/shims/buffer.js',
+      'path': 'browserfs/dist/shims/path.js',
+      'processGlobal': 'browserfs/dist/shims/process.js',
+      'bufferGlobal': 'browserfs/dist/shims/bufferGlobal.js',
+      'bfsGlobal': require.resolve('browserfs')
+    }
+  },
   plugins: [
+    new webpack.ProvidePlugin({ BrowserFS: 'bfsGlobal', process: 'processGlobal', Buffer: 'bufferGlobal' }),
     new MonacoWebpackPlugin()
   ]
 };
