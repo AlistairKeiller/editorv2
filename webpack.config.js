8f23ad731c8d554846c55bb0f90fc665618a861d
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-10-07 21:31:12 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,18 +14,8 @@ module.exports = {
       }
     ]
   },
-  resolve: {
-    alias: {
-      'fs': 'browserfs/dist/shims/fs.js',
-      'buffer': 'browserfs/dist/shims/buffer.js',
-      'path': 'browserfs/dist/shims/path.js',
-      'processGlobal': 'browserfs/dist/shims/process.js',
-      'bufferGlobal': 'browserfs/dist/shims/bufferGlobal.js',
-      'bfsGlobal': require.resolve('browserfs')
-    }
-  },
   plugins: [
-    new webpack.ProvidePlugin({ BrowserFS: 'bfsGlobal', process: 'processGlobal', Buffer: 'bufferGlobal' }),
-    new MonacoWebpackPlugin()
+    new MonacoWebpackPlugin(),
+    new webpack.ProvidePlugin({ BrowserFS: require.resolve('browserfs') })
   ]
 };
