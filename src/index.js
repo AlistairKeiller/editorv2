14551117a43c35fe79471097c71a1d00cf928914
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-10-11 08:15:54 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,23 +3,6 @@ import { WebrtcProvider } from 'y-webrtc';
 import { MonacoBinding } from 'y-monaco';
 import { editor } from 'monaco-editor';
 import * as Doppio from 'doppiojvm';
-const fs = BrowserFS.BFSRequire('fs'), path = BrowserFS.BFSRequire('path'), Buffer = BrowserFS.BFSRequire('buffer').Buffer, process = BrowserFS.BFSRequire('process');
-function copyDir(src, dest) {
-  fs.mkdir(dest, e => {
-    fs.readdir(src, (e, files) => {
-      files.forEach(file => {
-        var srcFile = path.resolve(src, file), destFile = path.resolve(dest, file);
-        fs.stat(srcFile, (e, stat) => {
-          stat.isDirectory() ?
-          copyDir(srcFile, destFile) :
-          fs.readFile(srcFile, (e, data) => {
-            fs.writeFile(destFile, data);
-          });
-        });
-      });
-    });
-  });
-}
 
 var ydoc = new Doc(),
 provider = new WebrtcProvider(window.location.pathname, ydoc),
@@ -42,20 +25,44 @@ monacoBinding = new MonacoBinding(
 fetch('doppio.zip')
   .then(r => r.arrayBuffer())
   .then(d => {
+    const fs = BrowserFS.BFSRequire('fs'), path = BrowserFS.BFSRequire('path'), Buffer = BrowserFS.BFSRequire('buffer').Buffer, process = BrowserFS.BFSRequire('process');
+
+    function copyDir(src, dest) {
+      fs.mkdir(dest, e => {
+        fs.readdir(src, (e, files) => {
+          files.forEach(file => {
+            var srcFile = path.resolve(src, file), destFile = path.resolve(dest, file);
+            fs.stat(srcFile, (e, stat) => {
+              stat.isDirectory() ?
+              copyDir(srcFile, destFile) :
+              fs.readFile(srcFile, (e, data) => {
+                fs.writeFile(destFile, data);
+              });
+            });
+          });
+        });
+      });
+    }
+
     process.initializeTTYs();
-    process.stdout.on('data', function(data) {
-      console.log(data.toString());
+    process.stdout.on('data', d => {
+      console.log(d.toString());
     });
-    process.stderr.on('data', function(data) {
-      console.log(data.toString());
+    process.stderr.on('data', d => {
+      console.log(d.toString());
     });
-  
+
     var mfs = new BrowserFS.FileSystem.MountableFileSystem();
     mfs.mount('/ziped', new BrowserFS.FileSystem.ZipFS(new Buffer(d)));
     mfs.mount('/home', new BrowserFS.FileSystem.InMemory());
     mfs.mount('/tmp', new BrowserFS.FileSystem.InMemory());
     BrowserFS.initialize(mfs);
     copyDir('/ziped', '/home');
+    
+    fs.readFile('/home/Javac.class', (e, d) => {
+      console.log(e);
+      console.log(d);
+    })
     
     var button = document.getElementById('loadButton');
     button.id = 'runButton';
