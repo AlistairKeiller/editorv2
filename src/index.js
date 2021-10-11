e11420a1960d88a68791c9221394cf45bae051fd
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-10-10 23:13:44 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import { Doc } from 'yjs';
 import { WebrtcProvider } from 'y-webrtc';
 import { MonacoBinding } from 'y-monaco';
 import { editor } from 'monaco-editor';
-import { VM } from 'doppiojvm';
-
+import * as Doppio from 'doppiojvm';
 const fs = BrowserFS.BFSRequire('fs'), path = BrowserFS.BFSRequire('path'), Buffer = BrowserFS.BFSRequire('buffer').Buffer, process = BrowserFS.BFSRequire('process');
-
 function copyDir(src, dest) {
   fs.mkdir(dest, e => {
     fs.readdir(src, (e, files) => {
@@ -65,12 +63,12 @@ fetch('doppio.zip')
       if (button.id === 'runButton'){
         button.id = 'runningButton';
         fs.writeFile('/tmp/Main.java', mEditor.getValue());
-        VM.CLI(
+        Doppio.VM.CLI(
           ['/home/Javac', '/tmp/Main.java'],
           {doppioHomePath: '/home'}, 
           e => {
             if (e === 0)
-              VM.CLI(
+              Doppio.VM.CLI(
                 ['/tmp/Main'],
                 {doppioHomePath: '/home'},
                 () => {button.id = 'runButton';}
