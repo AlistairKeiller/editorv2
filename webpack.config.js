273b29f8c444c9bc66c50b13bcb7295c1578ec5c
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-10-10 22:26:07 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new MonacoWebpackPlugin(),
-    new webpack.ProvidePlugin({ BrowserFS: require.resolve('browserfs'), fs: 'browserfs/dist/shims/fs.js', path: 'browserfs/dist/shims/path.js', Buffer: 'browserfs/dist/shims/buffer.js', process: 'browserfs/dist/shims/process.js' })
+    new MonacoWebpackPlugin()//,
+    //new webpack.ProvidePlugin({ BrowserFS: require.resolve('browserfs'), fs: 'browserfs/dist/shims/fs.js', path: 'browserfs/dist/shims/path.js', Buffer: 'browserfs/dist/shims/buffer.js', process: 'browserfs/dist/shims/process.js' })
   ]
 };
