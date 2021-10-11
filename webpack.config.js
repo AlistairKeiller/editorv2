3e4d7892963d528793fabde561035d9e9bd8e889
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-10-11 08:18:01 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,15 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js'
+    filename: 'app.js',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new MonacoWebpackPlugin(),
