725219e3dc0d865c68090960fe5e23c3acb5df43
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-10-11 09:02:20 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,6 @@ module.exports = {
   },
   plugins: [
     new MonacoWebpackPlugin(),
-    new webpack.ProvidePlugin({ BrowserFS: require.resolve('browserfs') })
+    new webpack.ProvidePlugin({ BrowserFS: 'browserfs' })
   ]
 };
