125d82c92028a16f3a4e022f2c20a6eeabc10499
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-10-13 08:45:59 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ fetch('doppio.zip')
                     console.log(d);
                   });
                   button.id = 'runningButton';
-                  VM.CLI(['/tmp/Main'], { doppioHomePath: '/home' }, () => {
+                  VM.CLI(['/tmp/Main'], { doppioHomePath: '/home', classpath: [ '/tmp' ] }, () => {
                     fs.unlink('/tmp/Main.class', () => {
                       button.id = 'runButton';
                     });
