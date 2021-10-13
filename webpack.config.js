c6633ddb3671db121a86df8217119250d3297a1f
AliCaliK1 <43255248+AliCaliK1@users.noreply.github.com>
2021-10-12 19:05:09 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin'), path = require('path'), webpack = require('webpack');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
